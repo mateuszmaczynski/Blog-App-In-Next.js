24e50f01610e8322447105145319a345c0428fb4
Mateusz Mączyński <mateuszmaczynski@gmail.com>
2021-03-25 19:32:14 +0100

    modify projects page (displaying projects from markdown files)

--- pages/projects.js
+++ pages/projects.js
@@ -1,13 +1,56 @@
 import Layout from 'components/Layout';
 import Head from 'next/head';
+import { getAllProjects } from 'services/projects';
 
-export default function Home() {
+export const getStaticProps = () => {
+  const projects = getAllProjects();
+
+  return {
+    props: { projects }
+  };
+};
+
+export default function Projects({ projects }) {
+
   return (
-    <div className="p-10 font-serif">
+    <Layout>
       <Head>
-        <title>My projects in next.js</title>
+        <title>Projects</title>
       </Head>
-      <Layout>Moje projekty</Layout>
-    </div>
+      <h1 className="text-center text-3xl mb-10">Projects</h1>
+      <ul className="mb-1">
+        {projects.map(({slug, title, tags, date, description}) => (
+          <li key={slug}>
+            <div className="relative pb-8">
+              <div className="relative flex items-start space-x-3">
+                <div className="min-w-0 flex-1 py-0">
+                  <div className="text-md text-gray-500">
+                    <div>
+                      <b className="font-medium text-gray-900 mr-2">{title}</b>
+                      {tags.map((tag, tagId) => (
+                        <span
+                          key={tagId}
+                          className="mr-2 my-0.5 relative inline-flex items-center bg-white rounded-full border border-gray-300 px-3 py-0.5 text-sm">
+                            <div className="absolute flex-shrink-0 flex items-center justify-center">
+                              <span
+                                className="h-1.5 w-1.5 rounded-full bg-green-500"
+                                aria-hidden="true"></span>
+                            </div>
+                            <div className="ml-3.5 font-medium text-gray-900">{tag}</div>
+                          </span>
+                      ))}
+                    </div>
+                    <span className="whitespace-nowrap text-sm">{date}</span>
+                  </div>
+                  <div className="mt-2 text-gray-700">
+                    <p>{description}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </Layout>
   );
 }
